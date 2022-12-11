a50d94b634d8363b640ebfc86d925a6c73daefe9
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-12-11 16:46:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ISC2O-UNIT5-04-CSharp-AND-OR
+
+[![Mr Coxall's Super Linter](https://github.com/Alex-Nelson-1/ISC2O-UNIT5-04-CSharp-AND-OR/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Alex-Nelson-1/ISC2O-UNIT5-04-CSharp-AND-OR/actions/)
+
+[![Run on Repl.it](https://repl.it/badge/github/Alex-Nelson-1/ISC2O-UNIT5-04-CSharp-AND-OR)](https://repl.it/github/Alex-Nelson-1/ISC2O-UNIT5-04-CSharp-AND-OR)
+
---
+++ main.cs
@@ -0,0 +1,40 @@
+// Created by: Alex Nelson
+// Created on: Nov 2022
+//
+// This program tells you which age range of movies you can
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Input
+        Console.WriteLine("Students between 12 and 21 recieve a discount to the museum on weekdays. Enter in your age and the day you're visiting the museum below ");
+        Console.WriteLine("");
+        Console.WriteLine("Please enter your age");
+        //input
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
+        Console.WriteLine("Please enter the day as a number(Monday being 1, and Sunday being 7)");
+        //input
+        int day = Convert.ToInt32(Console.ReadLine());
+        //Procces
+        Console.WriteLine("");
+        //Procces
+        if (day > 7)
+        {
+            Console.WriteLine("You have input something incorrectly. Please relaunch code.");
+        }
+        else if ((age > 12 && age < 21) && (day != 6 && day != 7))
+        {
+            //Output
+            Console.WriteLine("Eligible!");
+        }
+        //Output
+        else
+        {
+            Console.WriteLine("Not Eligible!");
+        }
+        Console.WriteLine("\nDone");
+    }
+}
