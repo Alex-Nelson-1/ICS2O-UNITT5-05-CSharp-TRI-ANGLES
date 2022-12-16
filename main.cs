7e46578e810426ad9ff89f82d81b406a98c9a2a4
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-12-16 00:43:56 +0000

    Comments + the original task

--- main.cs
+++ main.cs
@@ -8,33 +8,49 @@ class Program
 {
     public static void Main(string[] args)
     {
-        //Input
-        Console.WriteLine("Students between 12 and 21 recieve a discount to the museum on weekdays. Enter in your age and the day you're visiting the museum below ");
+        //Explanation
+        Console.WriteLine("This algorithm will be able to tell you the angles of any TRIANGLE. If the angles do not add up to 180 it is not a triangle.");
         Console.WriteLine("");
-        Console.WriteLine("Please enter your age");
-        //input
-        int age = Convert.ToInt32(Console.ReadLine());
+        
+        //input variables for side lengths 1, 2, and 3
+        Console.WriteLine("Please enter side length 1");
+        double lengthA = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
-        Console.WriteLine("Please enter the day as a number(Monday being 1, and Sunday being 7)");
-        //input
-        int day = Convert.ToInt32(Console.ReadLine());
-        //Procces
+        Console.WriteLine("Please enter side length 2");
+        double lengthB = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
-        //Procces
-        if (day > 7)
+        Console.WriteLine("Please enter side length 3");
+        double lengthC = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
+        
+        //The Acos process that determines the angles of the triangles
+        Console.WriteLine("");
+        double angleA = Math.Acos((lengthB*lengthB + lengthC*lengthC - lengthA*lengthA) / (2 * lengthB * lengthC)) * (180/Math.PI);
+  double angleB = Math.Acos((lengthC*lengthC + lengthA*lengthA - lengthB*lengthB) / (2 * lengthC * lengthA)) * (180/Math.PI);
+  double angleC = Math.Acos((lengthA*lengthA + lengthB*lengthB - lengthC*lengthC) / (2 * lengthA * lengthB)) * (180/Math.PI);
+        
+        //Sum of angels A, B, and C
+        double sumOfAngles = (angleA + angleB + angleC);
+        
+        //Output for every triangle
+        if (sumOfAngles != 180 && sumOfAngles != 180.00000000000003)
         {
-            Console.WriteLine("You have input something incorrectly. Please relaunch code.");
+            Console.WriteLine("Not a triangle.");
+            Console.WriteLine(sumOfAngles);
         }
-        else if ((age > 12 && age < 21) && (day != 6 && day != 7))
+        else if (angleA == angleB && angleB == angleC)
         {
-            //Output
-            Console.WriteLine("Eligible!");
+        Console.WriteLine("This is an equilateral triangle");
         }
-        //Output
-        else
+        else if (angleA == angleB || angleA == angleC || angleB == angleC)
         {
-            Console.WriteLine("Not Eligible!");
+        Console.WriteLine("This is an isoscolese triangle");
         }
+        else if (angleA != angleB && angleA != angleC && angleB != angleC)
+        {
+        Console.WriteLine("This is a scalene triangle");
+        }
+        //Done
         Console.WriteLine("\nDone");
     }
 }
